3f57aa2f01430720b4acbc993fc43777002a0b37
Danny Wahl <danny@iywahl.com>
2025-06-12 09:11:40 -0600

    wrap in settings provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
 import { View } from "@instructure/ui-view";
 import "./App.css";
+import { InstUISettingsProvider } from "@instructure/emotion";
 import Banner from "./components/Banner";
 import SignupModal from "./components/SignupModal";
 
 function App() {
 	return (
-		<View as="div">
-			<View as="section">
-				<Banner href="https://www.instructure.com/instructurecon/spokane" />
-			</View>
+		<InstUISettingsProvider>
+			<View as="div">
+				<View as="section">
+					<Banner href="https://www.instructure.com/instructurecon/spokane" />
+				</View>
 
-			<View
-				as="aside"
-				insetBlockEnd="0"
-				insetInlineEnd="0"
-				padding="medium"
-				position="fixed"
-			>
-				<SignupModal />
+				<View
+					as="aside"
+					insetBlockEnd="0"
+					insetInlineEnd="0"
+					padding="medium"
+					position="fixed"
+				>
+					<SignupModal />
+				</View>
 			</View>
-		</View>
+		</InstUISettingsProvider>
 	);
 }
 export default App;
