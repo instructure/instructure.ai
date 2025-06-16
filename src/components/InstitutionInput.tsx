94afe3c0a0c0a3501e9e3e9f9b022123f4cc4c9d
Danny Wahl <danny@iywahl.com>
2025-06-16 15:50:01 -0600

    institution validation

--- src/components/InstitutionInput.tsx
+++ src/components/InstitutionInput.tsx
@@ -1,23 +1,54 @@
+import type { FormMessage } from "@instructure/ui-form-field";
 import { IconHomeLine } from "@instructure/ui-icons";
 import { TextInput } from "@instructure/ui-text-input";
-import { useState } from "react";
+import React, { type SyntheticEvent, useState } from "react";
 
 type InstitutionInputProps = {
 	isDisabled?: boolean;
 };
 
 const InstitutionInput: React.FC<InstitutionInputProps> = ({ isDisabled }) => {
-	const [messages, setMessages] = useState([]);
-	const [showError, setShowError] = useState(false);
+	const [messages, setMessages] = useState<FormMessage[]>([]);
+	const [value, setValue] = useState<string>("");
+
+	const isInvalidInstitution = (name: string): boolean => {
+		const namePattern = /^[\p{L}\p{M}'’\-. ]+$/u;
+		const spacePattern = /^[ \t]+$/;
+		return !namePattern.test(name) || spacePattern.test(name);
+	};
+
+	const handleChange = (
+		_e: SyntheticEvent<Element, Event>,
+		value: string,
+	): void => {
+		setMessages([]);
+		setValue(value);
+	};
+
+	const handleBlur = (_e: SyntheticEvent<Element, Event>): void => {
+		let m: null | string = null;
+		if (value === "") {
+			m = "Institution is required.";
+		} else if (isInvalidInstitution(value)) {
+			m = "Please input full institution name.";
+		}
+		if (m) {
+			setMessages([{ text: m, type: "error" }]);
+		}
+	};
 
 	return (
 		<TextInput
 			disabled={isDisabled}
 			isRequired={true}
-			messages={showError ? messages : []}
+			messages={messages}
+			onBlur={handleBlur}
+			onChange={handleChange}
 			placeholder="Springfield Elementary"
 			renderBeforeInput={IconHomeLine}
 			renderLabel="Institution"
+			type="text"
+			value={value}
 		/>
 	);
 };
