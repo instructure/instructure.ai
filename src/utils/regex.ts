d5292bada7c486323d160ba1f68f2277fb4dab5e
Danny Wahl <danny@iywahl.com>
2025-06-18 08:49:16 -0600

    allow literal "&" in NamePattern

--- src/utils/regex.ts
+++ src/utils/regex.ts
@@ -2,7 +2,7 @@ const EmailAddressPattern: RegExp =
 	/^[\w.!#$%&'*+/=?^`{|}~-]+@[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?(?:\.[a-z\d](?:[a-z\d-]{0,61}[a-z\d])?)*$/i;
 const PersonalEmailPattern: RegExp =
 	/@(gmail|yahoo|outlook|hotmail|aol|icloud|mail|protonmail|zoho|yandex)\.com$/i;
-const NamePattern: RegExp = /^[\p{L}\p{M}'’\-. ]+$/u;
+const NamePattern: RegExp = /^[\p{L}\p{M}'’\-. &]+$/u;
 const SpacePattern: RegExp = /^[ \t]+$/;
 
 export { EmailAddressPattern, PersonalEmailPattern, NamePattern, SpacePattern };
