2b52c4b0dc161ea141fab5c2c404d863725368a7
Danny Wahl <danny@iywahl.com>
2025-07-02 17:45:02 -0600

    update logo for test route

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,5 @@
 import { Heading } from "@instructure/ui-heading";
+import { IconInstructureLogoLine } from "@instructure/ui-icons";
 import { Link } from "@instructure/ui-link";
 import { View } from "@instructure/ui-view";
 import { InstructureBugColor } from "../assets/Logos";
@@ -8,18 +9,24 @@ interface BannerProps {
 }
 
 const Banner = ({ href }: BannerProps): React.ReactElement => {
+	const isTestRoute =
+		typeof window !== "undefined" && window.location.hash === "#/test";
+	const LogoComponent = isTestRoute
+		? InstructureBugColor
+		: IconInstructureLogoLine;
+
 	return (
 		<View as="div" borderRadius="medium" overflowX="hidden" overflowY="hidden">
 			<View as="div" className="logoWrapper" padding="medium">
 				{href ? (
 					<Link href={href}>
 						<Heading as="h1" level="h1">
-							<InstructureBugColor />
+							<LogoComponent />
 						</Heading>
 					</Link>
 				) : (
-					<InstructureBugColor />
-				)}{" "}
+					<LogoComponent />
+				)}
 			</View>
 		</View>
 	);
