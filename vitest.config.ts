b03c98ba678620cadb438b940aa0b2320626d904
Danny Wahl <danny@iywahl.com>
2025-07-15 16:48:30 -0600

    update tests (#1)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  test: {
-    environment: 'jsdom',
-  },
+	plugins: [react()],
+	test: {
+		environment: "jsdom",
+		globals: true,
+		setupFiles: ["./tests/setup.ts"],
+	},
 });
