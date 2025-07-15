eb5be4f15f9720cdd5f995ca6582a891714f46b6
Danny Wahl <danny@iywahl.com>
2025-07-15 15:58:21 -0600

    Fixes #1
    inline Icon still has aria-hidden.

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,12 +1,10 @@
-import { AccessibleContent, IconExternalLinkLine, Link } from "@instructure/ui";
+import { IconExternalLinkLine, Link } from "@instructure/ui";
 
 type ExternalLinkProps = Omit<React.ComponentProps<typeof Link>, "target">;
 
 const ExternalLink: React.FC<ExternalLinkProps> = (props) => (
 	<Link {...props} target="_blank">
-		<AccessibleContent alt={props["aria-label"] || "External link"}>
-			{props.children} <IconExternalLinkLine />
-		</AccessibleContent>
+		{props.children} <IconExternalLinkLine />
 	</Link>
 );
 
