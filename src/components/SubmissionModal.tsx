d9d5d48bc05206bdbe5bf0cbdce589c510d06fcd
Danny Wahl <danny@iywahl.com>
2025-07-18 13:08:10 -0600

    update submission confirmation

--- src/components/SubmissionModal.tsx
+++ src/components/SubmissionModal.tsx
@@ -51,8 +51,12 @@ const SubmissionModal = ({
 			</Flex.Item>
 			<Flex.Item>
 				<Text as="p">
-					If you come across other features you're interested in, you can always
-					edit this form and resubmit.
+					We'll keep you informed on updates to these products over the coming
+					months.
+				</Text>
+				<Text as="p">
+					P.S. If you come across other features you're interested in, you can
+					always edit this form and resubmit.
 				</Text>
 			</Flex.Item>
 		</Flex>
