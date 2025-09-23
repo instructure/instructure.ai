1d602cf7dea0f1b5473927bb12a5dd0f39cfd1a0
Danny Wahl <danny@iywahl.com>
2025-09-23 08:38:28 -0600

    start AiInformation

---
+++ packages/nutritionfacts/src/assets/AiInformation.ts
@@ -0,0 +1,14 @@
+const AiInformation = [
+    {
+      featureName: "Feature name",
+      permissionLevelText: "Permission Level",
+      permissionLevel: "LEVEL 1",
+      description: "We leverage anonymized aggregate data for detailed analytics to inform model development and product improvements. No AI models are used at this level.",
+      permissionLevelsModalTriggerText: "Permission Levels",
+      modelNameText: "Base Model",
+      modelName: "Claude 3 Haiku",
+      nutritionFactsModalTriggerText: "AI Nutrition Facts"
+      }
+  ]
+
+  export { AiInformation } 
