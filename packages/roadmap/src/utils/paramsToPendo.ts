dcbd5e0f34d638d19c9282b1d017c3fa9b5397bd
Danny Wahl <danny@iywahl.com>
2025-09-23 14:56:23 -0600

    add paramstoPendo

---
+++ packages/roadmap/src/utils/paramsToPendo.ts
@@ -0,0 +1,68 @@
+const isPendoAPIFeature = (obj: any): obj is PendoAPIFeature => {
+  return (
+    obj &&
+    typeof obj === "object" &&
+    obj.feature &&
+    typeof obj.feature === "object" &&
+    "stage" in obj.feature &&
+    "title" in obj.feature &&
+    obj.product &&
+    typeof obj.product === "object" &&
+    "name" in obj.product &&
+    "area" in obj.product
+  );
+}
+
+const isPendoAPI = (obj: any): obj is PendoAPI => {
+  return (
+    obj &&
+    typeof obj === "object" &&
+    Array.isArray(obj.results) &&
+    obj.results.every(isPendoAPIFeature)
+  );
+}
+
+const paramsToPendo = (params: string | null): RoadmapFeatures | null => {
+  if (!params) return null;
+
+  let p: any;
+  try {
+    p = JSON.parse(decodeURIComponent(params));
+  } catch {
+    return null;
+  }
+
+  if (!isPendoAPI(p)) {
+    return null;
+  }
+
+  const stages = Array.from(new Set(p.results.map(result => result.feature.stage))).filter(Boolean) as string[];
+  const products = Array.from(new Set(p.results.map(result => result.product.name))).filter(Boolean) as string[];
+  const productAreas = Array.from(new Set(p.results.map(result => result.product.area))).filter(Boolean) as string[];
+  const labels = Array.from(new Set(p.results.flatMap(result => result.feature.labels))).filter(Boolean) as string[];
+  const features = p.results;
+
+  const roadmap: RoadmapFeatures = {
+    stages,
+    products,
+    productAreas,
+    labels,
+    features
+  };
+
+  console.log('Parsed roadmap:', roadmap);
+
+  if (
+    Array.isArray(roadmap.stages) &&
+    Array.isArray(roadmap.products) &&
+    Array.isArray(roadmap.productAreas) &&
+    Array.isArray(roadmap.labels) &&
+    Array.isArray(roadmap.features)
+  ) {
+    return roadmap;
+  }
+
+  return null;
+};
+
+export default paramsToPendo;
