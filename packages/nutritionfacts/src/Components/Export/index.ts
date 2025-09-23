fb7ffe161c05b918e1c9a4344b33981f2209635c
Danny Wahl <danny@iywahl.com>
2025-09-22 19:30:39 -0600

    fix barrel

--- packages/nutritionfacts/src/Components/Export/index.ts
+++ packages/nutritionfacts/src/Components/Export/index.ts
@@ -1,4 +1,4 @@
 export { ControlButton } from "./ControlButton";
 export { EmbedControl } from "./Embed";
 export { LinkControl } from "./Link";
-export { ObjectControl } from "./object";
+export { ObjectControl } from "./Object";
