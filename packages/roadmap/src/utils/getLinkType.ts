0aa1c7225e522936ba15791b415e55f91f963916
Danny Wahl <danny@iywahl.com>
2025-09-28 09:20:17 -0600

    better link sanitization

--- packages/roadmap/src/utils/getLinkType.ts
+++ packages/roadmap/src/utils/getLinkType.ts
@@ -6,32 +6,35 @@ type VerifiedLink = {
 const getLinkTitle = (link: VerifiedLink): string => {
   const { title, linkUrl } = link;
 
-  let url: string = ""
+  let url: URL;
 
   try {
-    url = new URL(linkUrl).href.toLowerCase();
+    url = new URL(linkUrl);
   } catch (error) {
     console.error("Invalid URL:", linkUrl, error);
     return title;
   }
 
+  const href = url.href.toLowerCase();
+  const host = url.hostname.toLowerCase();
+
   /* Community Links */
-  if (/change[-]?log/i.test(url)) return "Changelog";
-  if (/time[ ]?line/i.test(url)) return "Timeline";
-  if (url.includes("faq")) return "FAQ";
-  if (url.includes("the-product-blog")) return "Product Blog";
-  if (url.includes("guide")) return "Guides";
-  if (url.includes("community.canvaslms.com")) return "Community";
+  if (/change[-]?log/i.test(href)) return "changelog";
+  if (/time[ ]?line/i.test(href)) return "timeline";
+  if (href.includes("faq")) return "FAQ";
+  if (href.includes("the-product-blog")) return "Product Blog";
+  if (href.includes("guide")) return "Guides";
+  if (host.endsWith("community.canvaslms.com")) return "Community";
 
   /* Video Links */
-  if (url.match(/\.(mp4|mov|avi|mkv|mpeg)$/)) return "Video";
-  if (url.includes("instructuremedia.com")) return "Video";
-  if (url.includes("youtube.com")) return "Video";
-  if (url.includes("vimeo.com")) return "Video";
-  if (url.includes("wistia.com")) return "Video";
+  if (href.match(/\.(mp4|mov|avi|mkv|mpeg)$/)) return "Video";
+  if (host.endsWith("instructuremedia.com")) return "Video";
+  if (host.endsWith("youtube.com")) return "Video";
+  if (host.endsWith("vimeo.com")) return "Video";
+  if (host.endsWith("wistia.com")) return "Video";
 
   /* Image Links */
-  if (url.match(/\.(jpeg|jpg|gif|png|svg|webp)$/)) return "Image";
+  if (href.match(/\.(jpeg|jpg|gif|png|svg|webp)$/)) return "Image";
 
   return title;
 };
