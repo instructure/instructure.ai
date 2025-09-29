6a02599aea8810301cf495bfda95db0f2479a940
Danny Wahl <danny@iywahl.com>
2025-09-29 07:40:54 -0600

    lint

--- packages/roadmap/src/utils/getRoadmap.ts
+++ packages/roadmap/src/utils/getRoadmap.ts
@@ -3,17 +3,17 @@ import paramsToPendo from "./paramsToPendo";
 type RoadmapRequestEvent = MessageEvent<{ value?: string }>;
 
 const getRoadmap = (): Promise<RoadmapFeatures | null> => {
-    window.parent.postMessage({ type: "getRoadmap" }, "*");
+	window.parent.postMessage({ type: "getRoadmap" }, "*");
 
-    return new Promise((resolve) => {
-        const handler = (event: RoadmapRequestEvent) => {
-            if (event.data?.value) {
-                window.removeEventListener("message", handler);
-                resolve(paramsToPendo(event.data.value));
-            }
-        };
-        window.addEventListener("message", handler);
-    });
+	return new Promise((resolve) => {
+		const handler = (event: RoadmapRequestEvent) => {
+			if (event.data?.value) {
+				window.removeEventListener("message", handler);
+				resolve(paramsToPendo(event.data.value));
+			}
+		};
+		window.addEventListener("message", handler);
+	});
 };
 
 export default getRoadmap;
