a98360e2c96fbc93401006a0365481b943a1fca0
Danny Wahl <danny@iywahl.com>
2025-09-29 14:56:27 -0600

    use shared promise for concurrent calls

--- packages/roadmap/src/utils/getRoadmap.ts
+++ packages/roadmap/src/utils/getRoadmap.ts
@@ -1,28 +1,27 @@
 import paramsToPendo from "./paramsToPendo";
 
-let roadmapListenerAdded = false;
+let roadmapPromise: Promise<RoadmapFeatures | null> | null = null;
 
 type RoadmapRequestEvent = MessageEvent<{ value?: string }>;
 
 const getRoadmap = (): Promise<RoadmapFeatures | null> => {
+	if (roadmapPromise) {
+		return roadmapPromise;
+	}
 	window.parent.postMessage({ type: "getRoadmap" }, "*");
 
-	return new Promise((resolve) => {
-		if (roadmapListenerAdded) {
-			// Listener already added, do not add again
-			resolve(null);
-			return;
-		}
-		roadmapListenerAdded = true;
+	roadmapPromise = new Promise((resolve) => {
 		const handler = (event: RoadmapRequestEvent) => {
 			if (event.data?.value) {
 				window.removeEventListener("message", handler);
-				roadmapListenerAdded = false;
+
+				roadmapPromise = null;
 				resolve(paramsToPendo(event.data.value));
 			}
 		};
 		window.addEventListener("message", handler);
 	});
+	return roadmapPromise;
 };
 
 export default getRoadmap;
