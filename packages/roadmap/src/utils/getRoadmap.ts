8c663e8340252e63a13f5e0f0573fcee13f824a2
Danny Wahl <danny@iywahl.com>
2025-09-29 10:15:15 -0600

    prevent multiple listeners

--- packages/roadmap/src/utils/getRoadmap.ts
+++ packages/roadmap/src/utils/getRoadmap.ts
@@ -1,14 +1,22 @@
 import paramsToPendo from "./paramsToPendo";
 
+let roadmapListenerAdded = false;
+
 type RoadmapRequestEvent = MessageEvent<{ value?: string }>;
 
 const getRoadmap = (): Promise<RoadmapFeatures | null> => {
 	window.parent.postMessage({ type: "getRoadmap" }, "*");
 
 	return new Promise((resolve) => {
+		if (roadmapListenerAdded) {
+			// Listener already added, do not add again
+			return;
+		}
+		roadmapListenerAdded = true;
 		const handler = (event: RoadmapRequestEvent) => {
 			if (event.data?.value) {
 				window.removeEventListener("message", handler);
+				roadmapListenerAdded = false;
 				resolve(paramsToPendo(event.data.value));
 			}
 		};
