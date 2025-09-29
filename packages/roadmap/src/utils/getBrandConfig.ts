29134e064c17df13d6fa651a76bf533322222085
Danny Wahl <danny@iywahl.com>
2025-09-29 14:49:12 -0600

    handle concurrent requests

--- packages/roadmap/src/utils/getBrandConfig.ts
+++ packages/roadmap/src/utils/getBrandConfig.ts
@@ -10,26 +10,19 @@ type BrandConfig = Record<string, unknown>;
 
 type PageSettingsEvent = MessageEvent<{ pageSettings?: PageSettings }>;
 
-let brandConfigListenerAdded = false;
 let cachedBrandConfig: BrandConfig | null = null;
+let brandConfigPromise: Promise<BrandConfig> | null = null;
 
 const getBrandConfig = (): Promise<BrandConfig> => {
+	if (cachedBrandConfig !== null) {
+		return Promise.resolve(cachedBrandConfig);
+	}
+	if (brandConfigPromise) {
+		return brandConfigPromise;
+	}
 	window.parent.postMessage({ subject: "lti.getPageSettings" }, "*");
 
-	return new Promise((resolve, reject) => {
-		if (brandConfigListenerAdded) {
-			if (cachedBrandConfig !== null) {
-				resolve(cachedBrandConfig);
-			} else {
-				reject(
-					new Error(
-						"Brand config listener already added and no cached value available.",
-					),
-				);
-			}
-			return;
-		}
-		brandConfigListenerAdded = true;
+	brandConfigPromise = new Promise((resolve) => {
 		const handler = async (event: PageSettingsEvent) => {
 			if (event.data?.pageSettings) {
 				const url = event.data.pageSettings.active_brand_config_json_url;
@@ -41,16 +34,18 @@ const getBrandConfig = (): Promise<BrandConfig> => {
 					console.error("Failed to fetch or parse brand config:", error);
 				}
 				window.removeEventListener("message", handler);
-				brandConfigListenerAdded = false;
+
 				const result = event.data.pageSettings.use_high_contrast
 					? {}
 					: (brandConfig ?? {});
 				cachedBrandConfig = result;
+				brandConfigPromise = null;
 				resolve(result);
 			}
 		};
 		window.addEventListener("message", handler);
 	});
+	return brandConfigPromise;
 };
 
 export default getBrandConfig;
