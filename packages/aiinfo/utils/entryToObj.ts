063f675b2d13cf2d6fc9ea3b28d77e2526a2d81f
Danny Wahl <danny@iywahl.com>
2025-10-03 22:15:28 -0600

    fix ignore

--- packages/aiinfo/utils/entryToObj.ts
+++ packages/aiinfo/utils/entryToObj.ts
@@ -1,38 +1,39 @@
+// biome-ignore-all assist/source/useSortedKeys: Sane ordering for CSV columns
+
 import type { CSV } from "../types";
 
 const entryToObj = (entry: CSV[number]) => {
 	console.log("size of entry:", entry.length);
-	// biome-ignore assist/source/useSortedKeys: sane ordering for CSV columns
 	const obj = {
 		uid: entry[0],
 		revision: entry[1],
 		feature: {
-			description: entry[3],
 			name: entry[2],
+			description: entry[3],
 		},
 		model: {
+			name: entry[4],
+			description: entry[5],
+			trained: entry[6],
 			data: entry[7],
 			dataDescription: entry[8],
-			description: entry[5],
-			name: entry[4],
-			trained: entry[6],
 		},
 		compliance: {
+			retention: entry[9],
 			logging: entry[10],
 			loggingDescription: entry[11],
-			pii: entry[14],
-			piiDescription: entry[15],
 			regions: entry[12],
 			regionsDescription: entry[13],
-			retention: entry[9],
+			pii: entry[14],
+			piiDescription: entry[15],
 		},
 		outputs: {
-			guardrails: entry[19],
+			settings: entry[16],
 			human: entry[17],
 			humanDescription: entry[18],
-			outcomes: entry[21],
+			guardrails: entry[19],
 			risks: entry[20],
-			settings: entry[16],
+			outcomes: entry[21],
 		},
 		group: entry[22],
 		permissions: entry[23],
