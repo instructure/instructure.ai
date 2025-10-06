3691d8337d7bbca844cd1290d8b700e239f856f7
Danny Wahl <dwahl@instructure.com>
2025-10-06 10:51:29 -0600

    simplify map

--- packages/aiinfo/utils/entryToNutritionFacts.ts
+++ packages/aiinfo/utils/entryToNutritionFacts.ts
@@ -2,80 +2,37 @@ import { nutritionFactsStrings } from "../strings/index.js";
 import type { AiInfoFeature, Entry, NutritionFactsStrings } from "../types.js";
 
 const setData = (
-	data: NutritionFactsStrings["data"],
-	entry: Entry,
+  data: NutritionFactsStrings["data"],
+  entry: Entry,
 ): AiInfoFeature["NutritionFacts"]["data"] => {
-	const { model, compliance, outputs } = entry;
-	return [
-		{
-			blockTitle: data[0].blockTitle,
-			segmentData: [
-				{
-					value: model.name,
-					valueDescription: model.description,
-					...data[0].segmentData[0],
-				},
-				{
-					value: model.trained,
-					...data[0].segmentData[1],
-				},
-				{
-					value: model.dataDescription,
-					...data[0].segmentData[2],
-				},
-			],
-		},
-		{
-			blockTitle: data[1].blockTitle,
-			segmentData: [
-				{
-					value: compliance.retention,
-					...data[1].segmentData[0],
-				},
-				{
-					value: compliance.logging,
-					valueDescription: compliance.loggingDescription,
-					...data[1].segmentData[1],
-				},
-				{
-					value: compliance.regions,
-					valueDescription: compliance.regionsDescription,
-					...data[1].segmentData[2],
-				},
-				{
-					value: compliance.pii,
-					valueDescription: compliance.piiDescription,
-					...data[1].segmentData[3],
-				},
-			],
-		},
-		{
-			blockTitle: data[2].blockTitle,
-			segmentData: [
-				{
-					value: outputs.settings,
-					...data[2].segmentData[0],
-				},
-				{
-					value: outputs.human,
-					valueDescription: outputs.humanDescription,
-					...data[2].segmentData[1],
-				},
-				{
-					value: outputs.guardrails,
-					...data[2].segmentData[2],
-				},
-				{
-					value: outputs.risks,
-					...data[2].segmentData[3],
-				},
-				{
-					value: outputs.outcomes,
-					...data[2].segmentData[4],
-				},
-			],
-		},
-	];
+  const { model, compliance, outputs } = entry;
+  const valueMap = [
+    [
+      { value: model.name, valueDescription: model.description },
+      { value: model.trained },
+      { value: model.dataDescription },
+    ],
+    [
+      { value: compliance.retention },
+      { value: compliance.logging, valueDescription: compliance.loggingDescription },
+      { value: compliance.regions, valueDescription: compliance.regionsDescription },
+      { value: compliance.pii, valueDescription: compliance.piiDescription },
+    ],
+    [
+      { value: outputs.settings },
+      { value: outputs.human, valueDescription: outputs.humanDescription },
+      { value: outputs.guardrails },
+      { value: outputs.risks },
+      { value: outputs.outcomes },
+    ],
+  ];
+  return data.map((block, i) => ({
+    blockTitle: block.blockTitle,
+    segmentData: block.segmentData.map((seg, j) => ({
+      ...seg,
+      ...valueMap[i][j],
+    })),
+  }));
 };
 
 const entryToNutritionFacts = (
