fc5cea4abbdec3581babd2058077a6e324bbb284
Danny Wahl <dwahl@instructure.com>
2025-10-06 09:15:15 -0600

    start transformation

--- packages/aiinfo/utils/entryToPermissionLevels.ts
+++ packages/aiinfo/utils/entryToPermissionLevels.ts
@@ -1,14 +1,41 @@
-import type { AiInfoFeature } from "../types.js";
+import { permissionLevelsStrings } from "../strings";
+import type {
+	AiInfoFeature,
+	DataPermissionLevelsStrings,
+	Entry,
+} from "../types.js";
+
+const setHighlighted = (
+	data: DataPermissionLevelsStrings["data"],
+	level: Entry["permissions"],
+): DataPermissionLevelsStrings["data"] => {
+	return data.map((entry, idx) => ({
+		...entry,
+		highlighted: idx === Number(level) - 1,
+	}));
+};
 
 const entryToPermissionLevels = (
 	entry: Entry,
 ): AiInfoFeature["DataPermissionLevels"] => {
+	const {
+		feature: { name },
+		permissions,
+	} = entry;
+	const { en: s } = permissionLevelsStrings as {
+		en: DataPermissionLevelsStrings;
+	};
 
-	const permissions: AiInfoFeature["DataPermissionLevels"] = {
-
-	}
-
-	return permissions;
+	return {
+		closeButtonText: s.closeButtonText,
+		closeIconButtonScreenReaderLabel: s.closeIconButtonScreenReaderLabel,
+		currentFeature: name,
+		currentFeatureText: s.currentFeatureText,
+		data: setHighlighted(s.data, permissions),
+		modalLabel: s.modalLabel,
+		title: s.title,
+		triggerText: s.triggerText,
+	};
 };
 
 export { entryToPermissionLevels };
