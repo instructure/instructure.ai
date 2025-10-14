9cdfd3f3e978f480382fcacd35207dc8f748fb42
Danny Wahl <dwahl@instructure.com>
2025-10-14 16:59:11 -0600

    move register to loader script

--- scripts/loader.mjs
+++ scripts/loader.mjs
@@ -1,16 +1,17 @@
 import { readFile } from "node:fs/promises";
+import { register } from "node:module";
 import { extname } from "node:path";
-import { fileURLToPath } from "node:url";
+import { fileURLToPath, pathToFileURL } from "node:url";
 import { transformWithEsbuild } from "vite";
 
 const mtsExtensions = new Set([".ts", ".mts", ".cts", ".tsx"]);
 
+register(new URL(import.meta.url).pathname, pathToFileURL("./"));
 
 export async function resolve(specifier, context, nextResolve) {
 	const resolved = await nextResolve(specifier, context, nextResolve);
 	return resolved;
 }
-
 
 export async function load(url, context, nextLoad) {
 	const extension = extname(url);
