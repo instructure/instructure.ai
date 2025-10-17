a0a829e49c1bfd077b49b8265ce5101a697a06c2
Danny Wahl <dwahl@instructure.com>
2025-10-17 07:36:53 -0600

    revert loader to cjs

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,2 +1,2 @@
 /// <reference path="./instructure__browserslist-config-instui.d.ts" />
-/// <reference path="./instructure.ai__shared-configs-workspace.d.ts" />
+/// <reference path="./instructure.ai__shared-configs-scripts-workspace.d.ts" />
