2822ce176d2bb04d0dff2c9b2846e44cba1bab75
Danny Wahl <dwahl@instructure.com>
2025-11-04 13:45:28 +0100

    basic fake modal

--- apps/roadmap/src/components/cardOverlay.tsx
+++ apps/roadmap/src/components/cardOverlay.tsx
@@ -56,7 +56,6 @@ const CardOverlayContent: FC<{
 					borderRadius="large"
 					borderWidth="small"
 					margin={isDark ? "none" : "xx-small"}
-					padding="small"
 					shadow={isDark ? "none" : "above"}
 					themeOverride={{
 						backgroundPrimaryInverse: "#171f24",
@@ -65,65 +64,88 @@ const CardOverlayContent: FC<{
 					}}
 					width="100%"
 				>
-					<Flex
-						alignItems="start"
-						direction="column"
-						gap="small"
+					<View
+						as="div"
+						borderColor={isDark ? "secondary" : "primary"}
+						borderWidth="0 0 small"
+						padding="small"
+						themeOverride={{
+							borderColorPrimary: "#D7DADE",
+							borderColorSecondary: "#2A353F",
+						}}
 					>
-						<Flex.Item shouldGrow shouldShrink width="100%">
-							<Flex direction="row" gap="medium">
-								<Flex.Item shouldGrow shouldShrink>
-									<product.logo inline /> {product.name}
-								</Flex.Item>
-								<Flex.Item>
-									<IconButton
-										color={isDark ? "primary-inverse" : undefined}
-										onClick={handleClick}
-										screenReaderLabel="Close"
-										size="small"
-										withBackground={false}
-										withBorder={false}
-									>
-										<IconXLine />
-									</IconButton>
-								</Flex.Item>
-							</Flex>
-						</Flex.Item>
-						<Flex.Item>
-							<Heading variant="titleCardMini">{feature.title}</Heading>
-						</Flex.Item>
-						<Flex.Item>
-							<Flex direction="row" gap="xx-small" wrap="wrap">
-								<Flex.Item>
-									<Pill
-										color={feature.stage === "Coming Soon" ? "success" : "info"}
-										themeOverride={{
-											background: isDark ? "#0E1316" : "#fff",
-											infoColor: Colors.parchment,
-											successColor: Colors.mastery,
-										}}
-									>
-										<Text size="legend">{feature.stage}</Text>
-									</Pill>
-								</Flex.Item>
-								{(() => {
-									const areaParts = product.area?.split(" - ");
-									return areaParts?.[1] ? (
-										<Flex.Item>
-											<Pill
-												themeOverride={{
-													background: isDark ? "#0E1316" : "#fff",
-													primaryColor: "#9EA6AD",
-												}}
-											>
-												<Text size="legend">{areaParts[1]}</Text>
-											</Pill>
-										</Flex.Item>
-									) : null;
-								})()}
-							</Flex>
-						</Flex.Item>
-					</Flex>
+						<Flex alignItems="start" direction="column">
+							<Flex.Item shouldGrow shouldShrink width="100%">
+								<Flex direction="row">
+									<Flex.Item shouldGrow shouldShrink>
+										<product.logo inline />{" "}
+										<Text
+											color={isDark ? "primary-inverse" : "secondary"}
+											variant="contentSmall"
+										>
+											{product.name}
+										</Text>
+									</Flex.Item>
+									<Flex.Item>
+										<View>
+										<IconButton
+											color={isDark ? "primary-inverse" : undefined}
+											onClick={handleClick}
+											screenReaderLabel="Close"
+											size="small"
+											withBackground={false}
+											withBorder={false}
+											
+										>
+											<IconXLine />
+										</IconButton>
+										</View>
+									</Flex.Item>
+								</Flex>
+							</Flex.Item>
+							<Flex.Item>
+								<Heading margin="0 0 small" variant="titleCardMini">
+									{feature.title}
+								</Heading>
+							</Flex.Item>
+							<Flex.Item>
+								<Flex direction="row" gap="xx-small" wrap="wrap">
+									<Flex.Item>
+										<Pill
+											color={
+												feature.stage === "Coming Soon" ? "success" : "info"
+											}
+											themeOverride={{
+												background: isDark ? "#0E1316" : "#fff",
+												infoColor: Colors.parchment,
+												successColor: Colors.mastery,
+											}}
+										>
+											<Text size="legend">{feature.stage}</Text>
+										</Pill>
+									</Flex.Item>
+									{(() => {
+										const areaParts = product.area?.split(" - ");
+										return areaParts?.[1] ? (
+											<Flex.Item>
+												<Pill
+													themeOverride={{
+														background: isDark ? "#0E1316" : "#fff",
+														primaryColor: "#9EA6AD",
+													}}
+												>
+													<Text size="legend">{areaParts[1]}</Text>
+												</Pill>
+											</Flex.Item>
+										) : null;
+									})()}
+								</Flex>
+							</Flex.Item>
+						</Flex>
+					</View>
+					<View as="div" padding="small">
+						Bottom
+					</View>
 				</View>
 			</Mask>
 		</Portal>
