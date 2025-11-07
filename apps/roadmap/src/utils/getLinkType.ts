f518a08509579fa3f5441a867460792eecd2fd12
Danny Wahl <dwahl@instructure.com>
2025-11-07 15:06:31 -0700

    support guidde videos

--- apps/roadmap/src/utils/getLinkType.ts
+++ apps/roadmap/src/utils/getLinkType.ts
@@ -30,7 +30,7 @@ const getLinkTitle = (link: VerifiedLink): string => {
 	if (
 		href.match(/\.(mp4|mov|avi|mkv|mpeg)$/) ||
 		host.match(
-			/(?:^|\.)((instructuremedia|youtube|vimeo|wistia)\.com|youtu\.be)$/,
+			/(?:^|\.)((instructuremedia|youtube|vimeo|wistia|guidde)\.com|youtu\.be)$/,
 		)
 	)
 		return "video";
