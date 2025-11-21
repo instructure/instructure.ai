a94fe51b223b4c726ccde7c8244a1a7bd78937e2
Danny Wahl <dwahl@instructure.com>
2025-11-21 15:01:50 -0700

    lint

--- packages/aiinfo/utils/consts.ts
+++ packages/aiinfo/utils/consts.ts
@@ -1,7 +1,8 @@
 const CSVURL =
-	"https://docs.google.com/spreadsheets/d/e/2PACX-1vS_lbR3ZmU9adfQOgaOUvzET_tVR4AERgU_OEXGVFndVB6zNoArNZr-aQCs4HHAlGvX0QSjVor6nUvA/pub?gid=2000446087&single=true&output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_lbR3ZmU9adfQOgaOUvzET_tVR4AERgU_OEXGVFndVB6zNoArNZr-aQCs4HHAlGvX0QSjVor6nUvA/pub?gid=2000446087&single=true&output=csv";
 
-const SCHEMAURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_lbR3ZmU9adfQOgaOUvzET_tVR4AERgU_OEXGVFndVB6zNoArNZr-aQCs4HHAlGvX0QSjVor6nUvA/pub?gid=716417708&single=true&output=csv";
+const SCHEMAURL =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vS_lbR3ZmU9adfQOgaOUvzET_tVR4AERgU_OEXGVFndVB6zNoArNZr-aQCs4HHAlGvX0QSjVor6nUvA/pub?gid=716417708&single=true&output=csv";
 
 const TEMPLATE_PACKAGE = `import { Button } from "@instructure/ui";
 import type { AiInfoFeatureProps } from "../types";
