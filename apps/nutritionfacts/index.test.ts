afbce62b09eb2ff5483f7bb876e362178bab1655
Danny Wahl <dwahl@instructure.com>
2025-11-21 12:50:19 -0700

    update test

--- apps/nutritionfacts/index.test.ts
+++ apps/nutritionfacts/index.test.ts
@@ -16,7 +16,7 @@ describe("index.html", () => {
   it("should have correct meta tags", () => {
     const charset = document.querySelector("meta[charset]");
     expect(charset).not.toBeNull();
-    expect(charset?.getAttribute("charset")).toBe("utf8");
+    expect(charset?.getAttribute("charset")).toBe("utf-8");
 
     const viewport = document.querySelector('meta[name="viewport"]');
     expect(viewport).not.toBeNull();
