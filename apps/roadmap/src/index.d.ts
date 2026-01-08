ba047dd5ff1afedd2934f21d19671ee84b182330
Danny Wahl <dwahl@instructure.com>
2026-01-07 19:53:21 -0700

    update types

--- apps/roadmap/src/index.d.ts
+++ apps/roadmap/src/index.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="@instructure/ui" />
 
-type FC<P = {}> = import("react").FC<P>;
-
 interface SVGInfo {
   color: string;
   SVG: ReactNode;
@@ -43,7 +41,7 @@ interface PendoAPIFeature {
   product: {
     area: string | null;
     name: string;
-    logo: FC<SVGProps<SVGSVGElement>> | undefined;
+    logo: React.FC<SVGProps<SVGSVGElement>> | undefined;
     color: string | undefined;
   };
 }
@@ -57,6 +55,6 @@ interface RoadmapFeatures {
 }
 
 declare module "*.css" {
-  const classes: { [key: string]: string };
+  const classes: Record<string, string>;
   export default classes;
 }
