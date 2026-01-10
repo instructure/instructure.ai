e31a0d300c7831fac4f975117d97e11509bfc776
Danny Wahl <dwahl@instructure.com>
2026-01-10 12:20:17 -0700

    update exports

--- apps/roadmap/src/utils/index.ts
+++ apps/roadmap/src/utils/index.ts
@@ -1,10 +1,10 @@
-export { default as getBrandConfig } from "./getBrandConfig";
+export { default as frameResize } from "./frameResize";
+export { fetchBrandConfig, getBrandConfig } from "./getBrandConfig";
 export { default as getColor } from "./getColor";
 export { default as getLinkType } from "./getLinkType";
 export { default as getLogo } from "./getLogo";
 export { default as getProductArea } from "./getProductArea";
 export { default as getRoadmap } from "./getRoadmap";
-export { default as paramsToPendo } from "./paramsToPendo";
-export { default as frameResize } from "./frameResize";
 export { default as hideNavigationMenu } from "./hideNavigationMenu";
 export { default as hideRightSideWrapper } from "./hideRightSideWrapper";
+export { default as paramsToPendo } from "./paramsToPendo";
