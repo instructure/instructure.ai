481694be581228470d3ceb436b4e8365327c61ab
Danny Wahl <dwahl@instructure.com>
2026-01-12 09:51:19 -0700

    trim whitespace before eval

--- apps/roadmap/src/utils/getColor.ts
+++ apps/roadmap/src/utils/getColor.ts
@@ -3,7 +3,7 @@ import { Colors } from "../components";
 const BRAND_INDEX = 0;
 
 const getLogo = (product: PendoAPIFeature["product"]["name"]) => {
-  const brand = product.split(" ")[BRAND_INDEX].toLowerCase();
+  const brand = product.trim().split(" ")[BRAND_INDEX].toLowerCase();
   if (Object.hasOwn(Colors, brand)) {
     // oxlint-disable-next-line no-unsafe-type-assertion
     return Colors[brand as keyof typeof Colors];
--- apps/roadmap/src/utils/getLogo.ts
+++ apps/roadmap/src/utils/getLogo.ts
@@ -1,8 +1,11 @@
-import Logos from "../components/logos";
+import { Logos } from "../components/logos";
+
+const BRAND_INDEX = 0;
 
 const getLogo = (product: PendoAPIFeature["product"]["name"]) => {
-  const brand = product.split(" ")[0].toLowerCase();
-  if (brand in Logos) {
+  const brand = product.trim().split(" ")[BRAND_INDEX].toLowerCase();
+  if (Object.hasOwn(Logos, brand)) {
+    // oxlint-disable-next-line no-unsafe-type-assertion
     return Logos[brand as keyof typeof Logos];
   }
   return Logos.instructure;
