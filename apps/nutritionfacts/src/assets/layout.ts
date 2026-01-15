08a457fca6764b1e0a84fb01460b0f17c14d536b
Danny Wahl <dwahl@instructure.com>
2026-01-15 13:17:39 -0700

    fix Layout class

--- apps/nutritionfacts/src/assets/layout.ts
+++ apps/nutritionfacts/src/assets/layout.ts
@@ -1,20 +1,23 @@
-import type { PageLayout } from "../types";
+import { type PageLayout } from "../types";
 
 class Layout implements PageLayout {
   copyright: boolean;
   disclaimer: boolean;
   revision: boolean;
+  permissions: boolean;
 
   constructor(layout: PageLayout) {
     this.copyright = layout.copyright;
     this.disclaimer = layout.disclaimer;
     this.revision = layout.revision;
+    this.permissions = layout.permissions;
   }
 }
 
 const DefaultLayout = new Layout({
   copyright: true,
   disclaimer: true,
+  permissions: true,
   revision: true,
 } as PageLayout);
 
